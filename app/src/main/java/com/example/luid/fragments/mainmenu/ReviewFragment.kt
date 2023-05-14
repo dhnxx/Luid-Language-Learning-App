468aedc441af35597ce1b5da10d4dab558cb707c
Dhanco Mendoza <dhancomendoza@gmail.com>
2023-05-14 14:58:09 +0800

    Added Review feature

--- app/src/main/java/com/example/luid/fragments/mainmenu/ReviewFragment.kt
+++ app/src/main/java/com/example/luid/fragments/mainmenu/ReviewFragment.kt
@@ -1,5 +1,6 @@
 package com.example.luid.fragments.mainmenu
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,36 +10,80 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.luid.R
 import com.example.luid.adapters.ParentPhaseAdapter
-import com.example.luid.adapters.SwitchStateViewModel
+import com.example.luid.adapters.ReviewAdapter
 import com.example.luid.classes.ParentPhase
+import com.example.luid.classes.Review
 import com.example.luid.database.DBConnect
+import com.example.luid.database.DBConnect.Companion.engWord
+import com.example.luid.database.DBConnect.Companion.kapWord
+import com.example.luid.database.DBConnect.Companion.questions_tb
+import com.example.luid.database.DBConnect.Companion.tagWord
+import com.example.luid.database.DBConnect.Companion.vsbty
+
+
+// import com.example.luid.database.DBHelper
 
 class ReviewFragment : Fragment() {
-
-
-    //view model to store switch state
-    private lateinit var switchStateViewModel: SwitchStateViewModel
-
-    // for Review list display
     private lateinit var recyclerView: RecyclerView
     private val reviewList = ArrayList<ParentPhase>()
     private val adapter = ParentPhaseAdapter(reviewList)
+    private lateinit var dbHelper: DBConnect
+    private lateinit var data: List<Review>
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
-        val view =  inflater.inflate(R.layout.fragment_review, container, false)
-
+        val view = inflater.inflate(R.layout.fragment_review, container, false)
         recyclerView = view.findViewById(R.id.reviewRecyclerView)
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
 
-        //Database initialization
+        val reviewList = getword()
+        val adapter = ReviewAdapter(reviewList)
+        recyclerView.adapter = adapter
 
         return view
+    }
+
+    private fun getword(): List<Review> {
+
+        val list = mutableListOf<Review>()
+        val selectQuery = "SELECT * FROM $questions_tb WHERE $vsbty = 1"
+        val db = DBConnect(requireContext()).readableDatabase
+
+        var cursor: Cursor = db.rawQuery(selectQuery, null)
+
+        try {
+
+
+            if (cursor.moveToFirst()) {
+                val kapIndex = cursor.getColumnIndex(kapWord)
+                val engIndex = cursor.getColumnIndex(engWord)
+                val tagIndex = cursor.getColumnIndex(tagWord)
+                val vsbtyIndex = cursor.getColumnIndex(vsbty)
+
+                while (cursor.moveToNext()) {
+                    val kap = cursor.getString(kapIndex)
+                    val eng = cursor.getString(engIndex)
+                    val tag = cursor.getString(tagIndex)
+                    val vsbty = cursor.getInt(vsbtyIndex)
+
+                    if (vsbty == 1) {
+                        val review = Review(kap, eng, tag)
+                        list.add(review)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            cursor.close()
+            db.close()
+        }
+
+        return list
     }
 
 
