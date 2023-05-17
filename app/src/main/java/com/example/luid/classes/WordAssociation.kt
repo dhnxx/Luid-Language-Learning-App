eddfaf14e97691dc0af6d94669962d44eaca9649
Dhanco Mendoza <dhancomendoza@gmail.com>
2023-05-17 20:51:58 +0800

    Added Phase 1 layout and functionalities.

---
+++ app/src/main/java/com/example/luid/classes/WordAssociation.kt
@@ -0,0 +1,16 @@
+package com.example.luid.classes
+
+data class WordAssociationClass(
+    val question: String,
+    val correct: String,
+    val correctImg: Int,
+    val decoy1: String,
+    val decoy1Img: Int,
+    val decoy2: String,
+    val decoy2Img: Int,
+    val decoy3: String,
+    val decoy3Img: Int,
+)
+{
+
+}
