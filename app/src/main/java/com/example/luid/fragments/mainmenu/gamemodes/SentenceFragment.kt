d3fa660397aaecfec4e9ba922d1be7745ea78ba1
Dhancovic Ivan Mendoza <dhancomendoza@gmail.com>
2023-05-26 19:14:58 +0800

    Added Progress Bar to all phases

--- app/src/main/java/com/example/luid/fragments/mainmenu/gamemodes/SentenceFragment.kt
+++ app/src/main/java/com/example/luid/fragments/mainmenu/gamemodes/SentenceFragment.kt
@@ -1,6 +1,7 @@
 package com.example.luid.fragments.mainmenu.gamemodes
 
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -8,17 +9,19 @@ import com.example.luid.R
 import com.example.luid.adaptersQ.PhaseTwoAdapter
 import com.example.luid.classes.SentenceFragment
 
-
 class SentenceFragment : AppCompatActivity() {
 
     private lateinit var questionList: ArrayList<SentenceFragment>
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: PhaseTwoAdapter
+    private lateinit var progressBar: ProgressBar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sentencefragment)
+        progressBar = findViewById(R.id.progressBar)
+        progressBar.progress = 1
 
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.setHasFixedSize(true)
@@ -26,29 +29,29 @@ class SentenceFragment : AppCompatActivity() {
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         questionList = ArrayList()
         getQuestionList()
-        adapter = PhaseTwoAdapter(recyclerView, questionList)
+        adapter = PhaseTwoAdapter(recyclerView, questionList, progressBar)
         recyclerView.adapter = adapter
 
+        progressBar.max = questionList.size
+        println (progressBar.progress)
+        println (progressBar.max)
     }
-
-
     private fun getQuestionList() {
-    questionList.add(
-        SentenceFragment(
-            "What is Good Morning Neighbours in Kapampangan?", "Magandang Umaga Kapitbahay", R.drawable.home,
-
+        questionList.add(
+            SentenceFragment(
+                "What is Good Morning Neighbours in Kapampangan?",
+                "Magandang Umaga Kapitbahay",
+                R.drawable.home
+            )
         )
-
-    )
 
         questionList.add(
             SentenceFragment(
-                "What is Good Morning Neighbours in Kapampangan?", "Magandang Umaga Kapitbahay", R.drawable.home,
-
-                )
-
+                "What is Good Morning Neighbours in Kapampangan?",
+                "Magandang Umaga Kapitbahay",
+                R.drawable.home
+            )
         )
-
 
     }
 }
